3a4e20eb9252b27ba605802d2426ff947e5975ed
Artem <ki4igin1992@outlook.com>
2020-02-16 22:08:11 +0300

    Init Project

---
+++ LedBlink/code/main.c
@@ -0,0 +1,28 @@
+#define __AVR_ATmega32U4__
+#define F_CPU 8000000
+// Includes --------------------------------------------------------------------
+#include <avr/io.h>
+#include <util/delay.h>
+#include "main.h"
+// Private Typedef -------------------------------------------------------------
+
+// Private Macro ---------------------------------------------------------------
+#define LED_PORT PORTC
+#define LED_PIN  PIN7
+// Private Variables -----------------------------------------------------------
+
+// Private Function prototypes -------------------------------------------------
+
+// Functions -------------------------------------------------------------------
+int main(void)
+{
+  DDRC |= 1 << DDC7;
+
+  while (1)
+  {
+    _delay_ms(300);
+    LED_PORT |= 1 << LED_PIN;
+    _delay_ms(1000);
+    LED_PORT &= ~(1 << LED_PIN);
+  }
+}
